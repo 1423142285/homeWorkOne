afbe0be6715481af6d523ea12672c5c954fa71f5
1423142285 <1423142285@qq.com>
2020-01-19 15:25:53 +0800

    第二次提交，员工信息查询完成

---
+++ src/main/java/com/fh/entity/vo/TempSer.java
@@ -0,0 +1,69 @@
+package com.fh.entity.vo;
+
+import java.util.Date;
+
+public class TempSer {
+    private Integer id;
+    private String name;
+    private String jobName;
+    private String leaderName;
+    private double salary;
+    private Date entryTime;
+    private String deptName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Date getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(Date entryTime) {
+        this.entryTime = entryTime;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getLeaderName() {
+        return leaderName;
+    }
+
+    public void setLeaderName(String leaderName) {
+        this.leaderName = leaderName;
+    }
+}
